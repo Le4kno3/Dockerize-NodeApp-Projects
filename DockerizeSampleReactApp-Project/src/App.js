abe9708f249f99ad488d8be3583667af28049f6f
le4kno3 <jlelo403@gmail.com>
2022-07-23 20:35:47 +0530

    update

--- DockerizeSampleReactApp-Project/src/App.js
+++ DockerizeSampleReactApp-Project/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Demo React App for ECS Testing
+          Demo React App for Dockerize Testing
         </p>
         <a
           className="App-link"
